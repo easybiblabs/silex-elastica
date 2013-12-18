302c2781b06cc18c7ba1ce92e9ea4ae168ae253b
Nils Adermann <naderman@naderman.de>
2013-12-18 20:28:33 +0100

    Add service provider skeleton

---
+++ src/EasyBib/Service/Elastica/ElasticaServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * This file is part of easybib/silex-elastica
+ *
+ * (c) Imagine Easy Solutions, LLC
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author   Nils Adermann <naderman@naderman.de>
+ * @license  BSD-2-Clause
+ * @link     http://www.easybib.com
+ */
+
+namespace EasyBib\Service\Elastica;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+/**
+ * Exposes an Elastica client through Pimple to Silex
+ */
+class ElasticaServiceProvider implements ServiceProviderInterface
+{
+    public function register(Application $app)
+    {
+    }
+}
